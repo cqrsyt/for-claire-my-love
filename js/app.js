d0157ee2cc8b8c0333c0947b6238fa658e193e4b
cqrsyt <u3648221@connect.hku.hk>
2026-09-17 00:32:06 -0700

    Stop the password-page reload loop and force 行楷 / italic fonts.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,25 @@
 (function () {
-  var s = document.createElement("script");
-  s.src = "https://cdn.jsdelivr.net/gh/cqrsyt/for-claire-my-love@7aad0d602fda7812033b2bced3fb661b03b09df1/js/app.js";
-  s.async = false;
-  document.head.appendChild(s);
+  "use strict";
+
+  function attach(src, onload, onerror) {
+    var s = document.createElement("script");
+    s.src = src;
+    s.async = false;
+    if (onload) s.onload = onload;
+    if (onerror) s.onerror = onerror;
+    (document.body || document.documentElement).appendChild(s);
+    return s;
+  }
+
+  var loaded = false;
+  function ok() { loaded = true; }
+
+  attach(
+    "https://cdn.jsdelivr.net/gh/cqrsyt/for-claire-my-love@7aad0d602fda7812033b2bced3fb661b03b09df1/js/app.js",
+    ok,
+    function () {
+      if (loaded) return;
+      attach("https://cdn.jsdelivr.net/gh/cqrsyt/for-claire-my-love@7aad0d6/js/app.js", ok);
+    }
+  );
 })();
